b8585ae1507b27d97c72191d426b0a8f81d5edc8
+ <+>
2022-03-14 20:39:47 -0400

    commit tree 9924b35e8d80bff7193ded0b0230216d3f8ed122

--- main.js
+++ main.js
@@ -2,11 +2,42 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+
 // Your JavaScript code goes here!
+const error = document.getElementById('modal') 
+             error.setAttribute('class' , 'hidden')
+
+document.addEventListener('DOMContentLoaded', () => {
+  let hearts = document.querySelectorAll('.like-glyph')
+
+   for(const heart of hearts) {
+     heart.addEventListener('click', event => {
+       mimicServerCall()
+       .then(() => {
+         if(heart.innerText === EMPTY_HEART){
+           heart.innerText = FULL_HEART
+           heart.className = 'activated-heart'
+         }
+         else {
+           heart.innerText === FULL_HEART
+             heart.classList.remove('activated-heart')
 
 
-
-
+        
+         
+      
+          .catch((hearts) => {
+            error.setAttribute('class','show')
+          setTimeout(() => {
+            error.setAttribute('class', 'hidden')},3000
+           
+          )}
+          )}
+       }
+      )}
+     )}
+      })
+    
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
